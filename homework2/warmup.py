94a57cd6ead0e415faceebc281b3394c8a925db5
JacksonJW <jacksonjwatkins@gmail.com>
2017-09-28 18:33:49 -0700

    Setup

---
+++ homework2/warmup.py
@@ -0,0 +1,32 @@
+# change, strip_quotes, scramble, say, triples, powers,
+#                    interleave, Cylinder, make_crypto_functions, random_name
+
+def change(c):
+    return (c, c, c, c)
+
+def strip_quotes(s):
+    return s
+
+def scramble(s):
+    return s
+
+def say(s):
+    return s
+
+def triples(number):
+    return number
+
+def powers(base, maximum):
+    return base * maximum
+
+def interleave(array, *args):
+    return array + args
+
+def Cylinder(radius=1, height=1):
+    return radius + height
+
+def make_crypto_functions(s, key):
+    return s + key
+
+def random_name(gender, region):
+    return gender + region
