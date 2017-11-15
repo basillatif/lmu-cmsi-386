4ba04cda69f863b7bea0a658d6d5944009649d18
JacksonJW <jacksonjwatkins@gmail.com>
2017-11-14 19:51:27 -0800

    adding new say

--- homework3/say.cpp
+++ homework3/say.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <cassert>
 
@@ -28,32 +27,3 @@ int main () {
  assert(say("hello")("there")("buddy")() == "hello there buddy");
  cout << "All tests pass" <<endl;
 }
-=======
-  #include <vector>
-  #include <sstream>
-
-int main {
-  string say (string first_word) {
-  vector <string> words;
-
-  string say_more(string word) {
-  if (word == null) {
-    std::stringstream sayfct;
-    for (i = 0; i < words.size(); i++)
-    {
-      if (i != 0)
-      sayfct << ",";
-      sayfct << words[i];
-    }
-    std::string s = sayfct.str();
-    return words; //join replacement
-  }
-    words.pushback(word);
-    return say_more;
-  }
-  return say_more first_word
-  }
-}
-
-assert(say == "Hi there")
->>>>>>> c3e33706af670caf357ee6cf6b1546c94db3a5ad
